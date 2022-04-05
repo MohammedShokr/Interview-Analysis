10526f8a52c5510bb63b3209edfdeeceb63084a5
ShaimaaSHassanen <s-shaimaa_said@zewailcity.edu.eg>
2022-04-06 00:00:48 +0300

    Updated Scoring

--- FER.py
+++ FER.py
@@ -7,6 +7,64 @@ model = load_model("./expression_analysis/model")
 class_labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 face_classifier = cv2.CascadeClassifier('./expression_analysis/haarcascade_frontalface_default.xml')
 
+def scoring_expression(expression_weights):
+    score = 0
+    angry = expression_weights[0]
+    if angry < 0.1:
+        score += 10
+    elif angry < 0.3:
+        score += 5
+    elif angry < 0.5:
+        score += 1
+    disgust = expression_weights[1]
+    if disgust < 0.1:
+        score += 10
+    elif disgust < 0.3:
+        score += 5
+    elif disgust < 0.5:
+        score += 1
+    fear = expression_weights[2]
+    if fear < 0.1:
+        score += 10
+    elif fear < 0.3:
+        score += 5
+    elif fear < 0.5:
+        score += 1
+    happy = expression_weights[3]
+    if happy < 0.1:
+        score += 1
+    elif happy < 0.3:
+        score += 5
+    elif happy < 0.6:
+        score += 10
+    elif happy < 0.8:
+        score += 5
+    else:
+        score += 1
+    neutral = expression_weights[4]
+    if neutral < 0.1:
+        score += 1
+    elif neutral < 0.3:
+        score += 5
+    elif neutral < 0.8:
+        score += 10
+    else:
+        score += 5
+    sad = expression_weights[5]
+    if sad < 0.2:
+        score += 10
+    elif sad < 0.3:
+        score += 5
+    elif sad < 0.5:
+        score += 1
+    surprise = expression_weights[6]
+    if surprise < 0.3:
+        score += 10
+    elif surprise < 0.5:
+        score += 5
+    else:
+        score += 1
+    return round(score/7, 2)
 
 def face_detector(img):
     # Convert image to grayscale
@@ -26,27 +84,32 @@ def face_detector(img):
     return (x,w,y,h), roi_gray, img
 
 
-cap = cv2.VideoCapture(r"C:\Users\Administrator\PycharmProjects\GP_demo\A one minute TEDx Talk for the digital age - Woody Roseland - TEDxMileHigh.mp4")
+def analyze_face(video_file):
+    cap = cv2.VideoCapture(video_file)
+    c = 0
+    expression_matrix = [0, 0, 0, 0, 0, 0, 0]
+    frame_cnt = 0
+    while True:
+        c += 1
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if c % 30 == 0:
+            rect, face, image = face_detector(frame)
+            if np.sum([face]) != 0.0:
+                roi = face.astype("float") / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
 
-while True:
-    ret, frame = cap.read()
-    rect, face, image = face_detector(frame)
-    if np.sum([face]) != 0.0:
-        roi = face.astype("float") / 255.0
-        roi = img_to_array(roi)
-        roi = np.expand_dims(roi, axis=0)
+                # make a prediction on the ROI, then lookup the class
+                preds = model.predict(roi)[0]
+                expression_matrix += preds
+                frame_cnt += 1
 
-        # make a prediction on the ROI, then lookup the class
-        preds = model.predict(roi)[0]
-        label = class_labels[preds.argmax()]  
-        label_position = (rect[0] + int((rect[1]/2)), rect[2] + 25)
-        cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
-    else:
-        cv2.putText(image, "No Face Found", (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
-        
-    cv2.imshow("img",image)
-    if cv2.waitKey(1) == 13: #13 is the Enter Key
-        break
-        
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+    expression_weights = expression_matrix / frame_cnt
+    score = scoring_expression(expression_weights)
+    return score
+
+print(analyze_face("./interview samples/introduceyourself.webm"))

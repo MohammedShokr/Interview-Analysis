f1f3e168f222c948acb82aee0593549e91102335
MohammedShokr <s-mohammedyounis@zewailcity.edu.eg>
2022-04-22 01:52:28 +0200

    Added Coherence assessment to the UI

--- views/analysis.py
+++ views/analysis.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from FER import analyze_face
 from tone import analyze_tone
-
+from audio_processing import convert_video_to_audio
+from coherence_assessment import coherence_scoring
+from speech_to_text import short_speech_to_text
 def load_view():
     with st.sidebar:
         st.title("Analysis")
@@ -16,30 +18,39 @@ def load_view():
         if analyzeBtn:
             if uploaded_file:
                 if "Facial Analysis" in selections:
+                    st.header("FER")
                     with st.spinner("Facial expressions are being analyzed"):
                         FER_score, FER_weights = analyze_face("./test_interviews/"+uploaded_file.name)
                     st.write(f'The score based on face expression analysis is: {FER_score}')
                     with col3:
-                        st.write(f'angry: {round(FER_weights[0],2)}%')
+                        st.write(f'angry: {round(FER_weights[0],2)*100}%')
                         st.progress(FER_weights[0])
-                        st.write(f'disgust: {round(FER_weights[1],2)}%')
+                        st.write(f'disgust: {round(100*FER_weights[1],2)}%')
                         st.progress(FER_weights[1])
-                        st.write(f'fear: {round(FER_weights[2],2)}%')
+                        st.write(f'fear: {round(100*FER_weights[2],2)}%')
                         st.progress(FER_weights[2])
-                        st.write(f'happy: {round(FER_weights[3],2)}%')
+                        st.write(f'happy: {round(100*FER_weights[3],2)}%')
                         st.progress(FER_weights[3])
-                        st.write(f'neutral: {round(FER_weights[4],2)}%')
+                        st.write(f'neutral: {round(100*FER_weights[4],2)}%')
                         st.progress(FER_weights[4])
-                        st.write(f'sad: {round(FER_weights[5],2)}%')
+                        st.write(f'sad: {round(100*FER_weights[5],2)}%')
                         st.progress(FER_weights[5])
-                        st.write(f'surprise: {round(FER_weights[6],2)}%')
+                        st.write(f'surprise: {round(100*FER_weights[6],2)}%')
                         st.progress(FER_weights[6])
+                    
                 if "Tone Analysis" in selections:
+                    st.header("Tone")
                     with st.spinner("Tone expressions are being analyzed"):
                         tone_score, tone_weights = analyze_tone("./test_interviews/"+uploaded_file.name)
                     st.write(f'The score based on tone analysis is: {tone_score}')
+
                 if "English Text Coherence" in selections:
-                    st.write("⏳Analysis of English is being implemented")
-                
+                    st.header("English")
+                    with st.spinner("English coherence is being assessed"):
+                        audio_path = convert_video_to_audio("./test_interviews/"+uploaded_file.name)
+                        text = short_speech_to_text(audio_path)
+                        coherence_score = coherence_scoring(text)
+                        st.write(f'The Coherence percentage of English text: {round(coherence_score*100,2)}%')
+                        st.progress(coherence_score)
             else:
                 st.write("ERROR: No video found, please select a video and try again!")

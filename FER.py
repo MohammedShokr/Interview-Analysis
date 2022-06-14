706b2e0c4d668e2b6df25ac852f1a831eb65f821
ShaimaaSHassanen <s-shaimaa_said@zewailcity.edu.eg>
2022-06-14 22:42:41 +0300

    version edits

--- FER.py
+++ FER.py
@@ -3,12 +3,20 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import img_to_array
 import cv2
 
+# Loading the pre-trained model
 model = load_model("./expression_analysis/model")
+
+# Define class labels
 class_labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+
+# Initialize face detection model
 face_classifier = cv2.CascadeClassifier('./expression_analysis/haarcascade_frontalface_default.xml')
 
+# Define the scoring function
 def scoring_expression(expression_weights):
+    #''' This function takes a list of probabilities of each class and returns a score form 0 to 10'''
     score = 0
+    # Get each expression and add or subtract points based on the presence of the expression
     angry = expression_weights[0]
     if angry < 0.1:
         score += 10
@@ -78,25 +86,33 @@ def scoring_expression(expression_weights):
         score += 1
     return round(score/7, 2)
 
+# Define function for face detection
 def face_detector(img):
+    #''' This function takes an image and returns the face in the image'''
     # Convert image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Call the classifier to get the positions of the faces
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
     if faces is ():
-        return (0, 0, 0, 0), np.zeros((48, 48), np.uint8), img
-    
+        # If there are no faces return matrix of zeros
+        return np.zeros((48, 48), np.uint8)
+
+    # Crop the faces from the image
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
 
     try:
+        # Resize the face to be 48*48
         roi_gray = cv2.resize(roi_gray, (48, 48), interpolation  =  cv2.INTER_AREA)
     except:
-        return (x,w,y,h), np.zeros((48,48), np.uint8), img
-    return (x,w,y,h), roi_gray, img
+        return np.zeros((48,48), np.uint8)
+    return roi_gray
 
-
+# Define the FER analysis function
 def analyze_face(video_file):
+    #'''This function takes a video and returns the FER score and extra details for reports''' 
+    # Initialize the needed variables
     labels= []
     cap = cv2.VideoCapture(video_file)
     c = 0
@@ -108,26 +124,31 @@ def analyze_face(video_file):
         ret, frame = cap.read()
         total_frames+=1
         if not ret:
+            # When all frames are analysed stop the program
             break
+        # Analyse one frame out of each ten frames
         if c % 10 == 0:
-            rect, face, image = face_detector(frame)
+            # Detect the face in the frame
+            face = face_detector(frame)
             if np.sum([face]) != 0.0:
+                # Preprocessing of the face
                 roi = face.astype("float") / 255.0
                 roi = img_to_array(roi)
                 roi = np.expand_dims(roi, axis=0)
 
-                # make a prediction on the ROI, then lookup the class
+                # Make a prediction on the ROI, then lookup the class
                 preds = model.predict(roi)[0]
                 labels.append(class_labels[preds.argmax()])  
+                # Append all prediction lists to a large matrix
                 expression_matrix[frame_cnt]=preds.tolist()
                 frame_cnt += 1
 
     cap.release()
     cv2.destroyAllWindows()
+    # Get the average probability of each class
     expression_weights = np.mean(np.array(list(expression_matrix.values())), axis=0)
+    # Call the scoring function to give a score to that question
     score = scoring_expression(expression_weights)
+    # Get the most common expressions
     most_common_label = max(labels, key = labels.count)
-    print(most_common_label)
     return score*10, expression_matrix, expression_weights, total_frames
-
-#print(analyze_face("./test_interviews/test1.mp4"))
